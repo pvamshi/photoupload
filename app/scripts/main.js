ee5169bb104d69191425cd7ec78491afb9c964d3
vamshi <vamshik@kareo.com>
2014-06-20 03:00:25 -0700

    drag and drop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,59 @@
+'use strict';
+
+function displayFiles(files, evt) {
+    var result = '';
+    var file;
+    for (var i = 0; file = files[i]; i++) {
+        // if the file is not an image, continue
+        if (!file.type.match('image.*')) {
+            continue;
+        }
+
+        var reader = new FileReader();
+        reader.onload = (function(tFile) {
+            return function(evt) {
+                // var div = document.createElement('div');
+                $('#filesInfo').html('<img style="width: 300px;" src="' + evt.target.result + '" />');
+                $('#upload_image').hide();
+                // document.getElementById('filesInfo').innerHTML(div);
+            };
+        }(file));
+        reader.readAsDataURL(file);
+    }
+
+}
+
 function fileSelect(evt) {
     if (window.File && window.FileReader && window.FileList && window.Blob) {
         var files = evt.target.files;
+        displayFiles(files, evt);
 
-        var result = '';
-        var file;
-        for (var i = 0; file = files[i]; i++) {
-            // if the file is not an image, continue
-            if (!file.type.match('image.*')) {
-                continue;
-            }
-
-            reader = new FileReader();
-            reader.onload = (function (tFile) {
-                return function (evt) {
-                    var div = document.createElement('div');
-                    div.innerHTML = '<img style="width: 90px;" src="' + evt.target.result + '" />';
-                    document.getElementById('filesInfo').appendChild(div);
-                };
-            }(file));
-            reader.readAsDataURL(file);
-        }
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
 }
 
-document.getElementById('filesToUpload').addEventListener('change', fileSelect, false); 
+function fileDropped(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        var files = evt.dataTransfer.files;
+
+        displayFiles(files, evt);
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+}
+
+
+function dragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+}
+
+var dropTarget = document.getElementById('dropTarget');
+dropTarget.addEventListener('dragover', dragOver, false);
+dropTarget.addEventListener('drop', fileDropped, false);
+
+document.getElementById('filesToUpload').addEventListener('change', fileSelect, false);
